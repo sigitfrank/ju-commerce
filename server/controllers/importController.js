8b2149057af41ead408185dcb7b62075553d1665
Sigit <sigittt53@gmail.com>
2022-01-12 09:20:57 +0700

    fix import controller

--- server/controllers/importController.js
+++ server/controllers/importController.js
@@ -18,15 +18,30 @@ const handleImport = async (request, h) => {
                 const { prdNo } = data
                 axios.get(`http://api.elevenia.co.id/rest/prodservices/product/details/${prdNo}`, axiosConfig).then(detailResponse => {
                     const detailProduct = parser.parse(detailResponse.data)
-                    const queryInsert = {
-                        text: 'INSERT INTO products (sku, name, image, price, description, created_at) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
-                        values: [detailProduct.Product.sellerPrdCd, detailProduct.Product.prdNm, detailProduct.Product.prdImage01, detailProduct.Product.selPrc, detailProduct.Product.htmlDetail, new Date()],
+
+                    const queryGet = {
+                        text: 'SELECT sku FROM products WHERE sku = $1',
+                        values: [detailProduct.Product.sellerPrdCd],
                     }
-                    pool.query(queryInsert).then(r => {
+                    pool.query(queryGet).then(r => {
+                        const isExist = r.rows.length
+                        if (isExist < 1) {
+                            const queryInsert = {
+                                text: 'INSERT INTO products (sku, name, image, price, description, created_at) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+                                values: [detailProduct.Product.sellerPrdCd, detailProduct.Product.prdNm, detailProduct.Product.prdImage01, detailProduct.Product.selPrc, detailProduct.Product.htmlDetail, new Date()],
+                            }
+                            pool.query(queryInsert).then(r => {
+                            }).catch(e => {
+                                // console.log(e)
+                            })
+                        }
                     }).catch(e => {
+                        // console.log(e)
                     })
                 })
             }
+        }).catch(er => {
+            // console.log(er)
         })
     }
 
